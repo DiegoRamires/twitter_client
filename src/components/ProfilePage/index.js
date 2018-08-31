c4cd3cd17a3d6ddfeeaa62f6a15febb85f802801
Diego Ramires <goncalves.ramires@gmail.com>
2018-08-31 19:21:25 -0300

    Create TweetyListContainer, TweetUnity and updated profilePage

--- src/components/ProfilePage/index.js
+++ src/components/ProfilePage/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Row, Col } from 'react-materialize';
 import UserInfoContainer from '../../containers/UserInfoContainer'
+import TweetListContainer from '../../containers/TweetListContainer'
 import MainLayout from '../MainLayout'
 
 const ProfilePage = () => (
    <MainLayout>
      <Row>
        <Col s={12} m={3}>
-         <UserInfoContainer/>
+      <UserInfoContainer/>
        </Col>
        <Col s={12} m={6}>
-        Tweet List
+         <TweetListContainer/>
        </Col>
        <Col s={12} m={3}>
         Trending Topics
