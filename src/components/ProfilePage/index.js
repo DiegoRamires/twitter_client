e489d170bd9574796e8b97a78a5e6e67bd3cc82f
Diego Ramires <goncalves.ramires@gmail.com>
2018-08-31 18:04:59 -0300

    Create userInfo with avatar and sidebar information

--- src/components/ProfilePage/index.js
+++ src/components/ProfilePage/index.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { Row, Col } from 'react-materialize';
+import UserInfoContainer from '../../containers/UserInfoContainer'
 import MainLayout from '../MainLayout'
 
 const ProfilePage = () => (
-  <MainLayout>
-    <Row>
-      <Col s={12} m={3}>
-        User Info
-      </Col>
-      <Col s={12} m={6}>
+   <MainLayout>
+     <Row>
+       <Col s={12} m={3}>
+         <UserInfoContainer/>
+       </Col>
+       <Col s={12} m={6}>
         Tweet List
-      </Col>
-      <Col s={12} m={3}>
+       </Col>
+       <Col s={12} m={3}>
         Trending Topics
-      </Col>
-    </Row>
-  </MainLayout>
+       </Col>
+     </Row>
+   </MainLayout>
 );
 
 export default ProfilePage;
